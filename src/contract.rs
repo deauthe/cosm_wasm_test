da6f40f31027c58251dce9f14851a4d450fabde7
deauth <anurage66@gmail.com>
2024-04-04 22:08:43 +0530

    Initial Commit

---
+++ src/contract.rs
@@ -0,0 +1,221 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use andromeda_std::{
+    ado_base::InstantiateMsg as BaseInstantiateMsg,
+    ado_contract::{
+        ADOContract,
+    },
+    common::{actions::call_action, context::ExecuteContext},
+    error::ContractError,
+};
+
+use crate::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
+use crate::state::{State, STATE};
+
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:project-name";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    let state = State {
+        count: msg.count,
+        owner: info.sender.clone(),
+    };
+    
+    STATE.save(deps.storage, &state)?;
+
+    let contract = ADOContract::default();
+
+    let resp = contract.instantiate(
+        deps.storage,
+        env,
+        deps.api,
+        &deps.querier,
+        info.clone(),
+        BaseInstantiateMsg {
+            ado_type: CONTRACT_NAME.to_string(),
+            ado_version: CONTRACT_VERSION.to_string(),
+            kernel_address: msg.kernel_address,
+            owner: msg.owner,
+        },
+    )?;
+
+    Ok(resp
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender)
+        .add_attribute("count", msg.count.to_string()))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> Result<Response, ContractError> {
+    let ctx = ExecuteContext::new(deps, info, env);
+    if let ExecuteMsg::AMPReceive(pkt) = msg {
+        ADOContract::default().execute_amp_receive(
+            ctx,
+            pkt,
+            handle_execute,
+        )
+    } else {
+        handle_execute(ctx, msg)
+    }
+}
+
+pub fn handle_execute(
+    mut ctx: ExecuteContext,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    let action_response = call_action(
+        &mut ctx.deps,
+        &ctx.info,
+        &ctx.env,
+        &ctx.amp_ctx,
+        msg.as_ref(),
+    )?;
+
+    let res = match msg {
+        ExecuteMsg::Increment {} => execute::increment(ctx),
+        ExecuteMsg::Reset { count } => execute::reset(ctx, count),
+        _ => ADOContract::default().execute(ctx, msg)
+    }?;
+
+    Ok(res
+        .add_submessages(action_response.messages)
+        .add_attributes(action_response.attributes)
+        .add_events(action_response.events))
+}
+
+
+pub mod execute {
+    use super::*;
+
+    pub fn increment(ctx: ExecuteContext) -> Result<Response, ContractError> {
+        let ExecuteContext { deps, .. } = ctx;
+        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+            state.count += 1;
+            Ok(state)
+        })?;
+
+        Ok(Response::new().add_attribute("action", "increment"))
+    }
+
+    pub fn reset(ctx: ExecuteContext, count: i32) -> Result<Response, ContractError> {
+        let ExecuteContext { deps, info, .. } = ctx;
+        STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+            if info.sender != state.owner {
+                return Err(ContractError::Unauthorized {});
+            }
+            state.count = count;
+            Ok(state)
+        })?;
+        Ok(Response::new().add_attribute("action", "reset"))
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> Result<Binary, ContractError> {
+    match msg {
+        QueryMsg::GetCount {} => Ok(to_json_binary(&query::count(deps)?)?),
+        _ => ADOContract::default().query(deps, env, msg),
+    }
+}
+
+pub mod query {
+    use super::*;
+
+    pub fn count(deps: Deps) -> Result<GetCountResponse, ContractError> {
+        let state = STATE.load(deps.storage)?;
+        Ok(GetCountResponse { count: state.count })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use andromeda_std::testing::mock_querier::MOCK_KERNEL_CONTRACT;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{coins, from_json};
+
+    #[test]
+    fn proper_initialization() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {
+            count: 17,
+            kernel_address: MOCK_KERNEL_CONTRACT.to_string(),
+            owner: None,
+        };
+        let info = mock_info("creator", &coins(1000, "earth"));
+
+        // we can just call .unwrap() to assert this was a success
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+
+        // it worked, let's query the state
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+        let value: GetCountResponse = from_json(&res).unwrap();
+        assert_eq!(17, value.count);
+    }
+
+    #[test]
+    fn increment() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {
+            count: 17,
+            kernel_address: MOCK_KERNEL_CONTRACT.to_string(),
+            owner: None,
+        };
+        let info = mock_info("creator", &coins(2, "token"));
+        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        // beneficiary can release it
+        let info = mock_info("anyone", &coins(2, "token"));
+        let msg = ExecuteMsg::Increment {};
+        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        // should increase counter by 1
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+        let value: GetCountResponse = from_json(&res).unwrap();
+        assert_eq!(18, value.count);
+    }
+
+    #[test]
+    fn reset() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {
+            count: 17,
+            kernel_address: MOCK_KERNEL_CONTRACT.to_string(),
+            owner: None,
+        };
+        let info = mock_info("creator", &coins(2, "token"));
+        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        // beneficiary can release it
+        let unauth_info = mock_info("anyone", &coins(2, "token"));
+        let msg = ExecuteMsg::Reset { count: 5 };
+        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
+        match res {
+            Err(ContractError::Unauthorized {}) => {}
+            _ => panic!("Must return unauthorized error"),
+        }
+
+        // only the original creator can reset the counter
+        let auth_info = mock_info("creator", &coins(2, "token"));
+        let msg = ExecuteMsg::Reset { count: 5 };
+        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+
+        // should now be 5
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+        let value: GetCountResponse = from_json(&res).unwrap();
+        assert_eq!(5, value.count);
+}
+}
